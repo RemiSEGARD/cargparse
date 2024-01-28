7df6fe202ddb59adcd82fdc09f89099cad3b6ad5
Remi SEGARD <remi.segard@epita.fr>
2024-01-28 02:06:41 +0100

    [cargparse][docs] document all functions

--- cargparse.h
+++ cargparse.h
@@ -4,58 +4,134 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+/**
+ * @def CARG_LOCATION is a macro that should be equal to a path leading to the
+ *      file containing all the ARGUMENT macros. Ideally an absolute path.
+ */
 #ifndef CARG_LOCATION
 #  error "CARG_LOCATION is not defined"
 #endif
 
+/**
+ * @brief Necessary typedef in order to get string arguments
+ */
 typedef char *cstr;
+
+/**
+ * @brief array of strings
+ */
 typedef struct
 {
-    char **strs;
-    size_t size;
-    size_t capacity;
+    char **strs; ///< The strings
+    size_t size; ///< The number of strings
+    size_t capacity; ///< The capacity of strs
 } cargparse_str_vector;
 
+/**
+ * @brief shorthand for easier argument declaration, maybe not necessary...
+ */
 typedef cargparse_str_vector str_vec;
 
+/**
+ * @brief Macro used to declare the arguments
+ *
+ * @details This macro is redefined multiple times in order to do different
+ *          things when including CARG_LOCATION, such as registering the arguments,
+ *          getting their stringified type, CLI name or description...
+ */
 #define ARGUMENT(NAME, TYPE, DEFAULT_VALUE, ARGS, DESC) TYPE NAME;
 #include CARG_LOCATION
 #undef ARGUMENT
 
+/**
+ * @brief adds a string to a str_vector
+ *
+ * @param vec the vector
+ * @param str the string
+ */
 void cargparse_str_vector_add_str(cargparse_str_vector *vec, char *str);
+/**
+ * @brief get the string at a given index in the vector
+ *
+ * @param vec the vector
+ * @param index the index
+ */
 char *cargparse_str_vector_get(cargparse_str_vector *vec, size_t index);
 
+/**
+ * @brief string view implemention
+ */
 typedef struct
 {
-    const char *str;
-    size_t size;
+    const char *str; ///< the string, possibly not NULL terminated
+    size_t size; ///< the size of the string
 } cargparse_str_view;
 
+/**
+ * @brief compares two string views
+ * 
+ * @param s1 the first string view
+ * @param s2 the other string view
+ */
 int cargparse_str_view_eq(const cargparse_str_view *s1,
                           const cargparse_str_view *s2);
+/**
+ * @brief creates a string view from a NULL terminated C string
+ *
+ * @param cstr the C string
+ */
 cargparse_str_view cargparse_str_view_from_cstr(const char *cstr);
+
+/**
+ * @brief splits the string into multiple string views. When there is not more
+ *        possible slices, the pointer insed the return value will be equal to
+ *        NULL
+ *
+ * @param ptr a pointer to a C string, will be modified in order to move inside
+ *        the string and remember the last position
+ * @param delim a string containing all possible delimiters to delimit slices
+ */
 cargparse_str_view cargparse_cstr_split(char **ptr, const char *delim);
 
+/**
+ * @brief a function type to parse an argument and set the value in a given
+ *        pointer
+ */
 typedef int(*cargparse_parse_type_arg_f)(char *, void *);
+/**
+ * @brief an item inside a argument hashmap
+ */
 typedef struct cargparse_arg_map_item
 {
-    cargparse_str_view name;
-    cargparse_parse_type_arg_f parse_function;
-    void *data;
-    int needs_value;
-    struct cargparse_arg_map_item *next;
+    cargparse_str_view name; ///< The CLI argument
+    cargparse_parse_type_arg_f parse_function; ///< the function to parse the 
+                                               ///value
+    void *data; ///< a pointer to the argument's data
+    int needs_value; ///< Wether the argument needs to consume an argument for
+                     ///its value
+    struct cargparse_arg_map_item *next; ///< a pointer to handle collisions
+                                         ///inside the map
 } cargparse_arg_map_item ;
 
+/**
+ * @brief gets an arg_map_item inside the global argument map
+ */
 cargparse_arg_map_item *
 cargparse_arg_map_item_get(const cargparse_str_view *arg_name);
 
+/**
+ * @brief argument hash map type
+ */
 typedef struct
 {
-    cargparse_arg_map_item **items;
-    size_t capacity;
+    cargparse_arg_map_item **items; ///< all the map items
+    size_t capacity; ///< the length of the items array
 } cargparse_arg_map;
 
 
+/**
+ * @brief all possible argument parsing errors
+ */
 typedef enum
 {
     CARGPARSE_NO_ERROR,
@@ -64,28 +140,83 @@ typedef enum
     CARGPARSE_PRINT_HELP,
 } cargparse_error;
 
+/**
+ * @brief checks if two C strings are equal
+ *
+ * @param s1 the first string
+ * @param s2 the other string
+ */
 static inline int cargparse_streq(const char *s1, const char *s2);
 
+/**
+ * @brief creates a argument map item and adds it to the global argument map
+ *
+ * @param arg the CLI argument
+ * @param parse_function the function to parse the argument's value
+ * @param data the data pointing to the argument's data
+ */
 int cargparse_register_arg(const cargparse_str_view *arg,
                            cargparse_parse_type_arg_f parse_function,
                            void *data);
+/**
+ * @brief prints the help message and exits
+ *
+ * @param error the error that lead to calling this function
+ * @param the argument that caused the error
+ */
 void cargparse_print_help(cargparse_error error, const char *wrong_arg);
 
+/**
+ * @brief functions used to parse a given type of argument
+ */
 int cargparse_parse_str_vec_arg(char *arg, void *data);
 int cargparse_parse_bool_arg(char *arg, void *data);
 int cargparse_parse_cstr_arg(char *arg, void *data);
 int cargparse_parse_int_arg(char *arg, void *data);
 
+/**
+ * @brief setups the arguments default value and registers them inside the map
+ *
+ * @param usage_str the usage string to print in the help message
+ */
 int cargparse_setup_args(char *usage_str);
 
+/**
+ * @brief shifts the arguments and returns the shifted argument
+ */
 char *cargparse_shift_args(int *argc, char **argv[]);
+/**
+ * @brief gets the name of the argument inside a string
+ *
+ * @param str the string the get the name from
+ * @param is_equal_arg is set to 1 if the name is followed by an '=' character
+ */
 cargparse_str_view cargparse_get_arg_name(const char *str, int *is_equal_arg);
+/**
+ * @brief parses a list of "small" arguments (-xa...)
+ *
+ * @param arg the list of arguments
+ * @param argc pointer to the remaining number of argument
+ * @param argv pointer to the remaining arguments
+ */
+int cargparse_parse_small_arg(char *arg, int *argc, char **argv[]);
+/**
+ * @brief parses an argument
+ *
+ * @param arg the argument to parse
+ * @param argc the remaming number of arguments
+ * @param argv the array of remaining arguments
+ */
 int cargparse_parse_argument(char *arg, int *argc, char **argv[]);
+/**
+ * @brief parses CLI arguments and sets the values of arguments
+ *
+ * @param pointer to main's argc, may get modified
+ * @param pointer to main's argv, may get modified
+ */
 int cargparse_parse_args(int *argc, char **argv[]);
 
-#ifndef CARGPARSE_IMPLEMENTATION
-
-#else /* CARGPARSE_IMPLEMENTATION */
+#ifdef CARGPARSE_IMPLEMENTATION /* CARGPARSE_IMPLEMENTATION */
 
 #undef ARGUMENT
 #define MIN(X, Y) ((X) < (Y) ? (X) : (Y))
@@ -400,5 +531,5 @@ int cargparse_parse_args(int *argc, char **argv[])
     return 0;
 }
 
-#endif /* ! CARGPARSE_IMPLEMENTATION */
+#endif /* CARGPARSE_IMPLEMENTATION */
 #endif /* ! CARGPARSE_H */
--- examples/example.c
+++ examples/example.c
@@ -9,10 +9,10 @@ int main(int argc, char *argv[])
     cargparse_setup_args("example [OPTIONS|ARGS...]");
     cargparse_parse_args(&argc, &argv);
 
-    printf("--> arg1 = %d!\n", arg1);
-    printf("--> arg2 = %s!\n", arg2);
-    printf("--> boolean = %i!\n", arg3);
-    printf("--> strs = (size %lu)", arg4.size);
+    printf("- arg1 = %d!\n", arg1);
+    printf("- arg2 = %s!\n", arg2);
+    printf("- boolean = %i!\n", arg3);
+    printf("- strs = (size %lu)", arg4.size);
     if (arg4.size > 0)
     {
         printf("%s", cargparse_str_vector_get(&arg4, 0));
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
     }
     printf("\n");
 
-    printf("Remaining arguments: %s", argv[0]);
+    printf("-> Remaining arguments: %s", argv[0]);
     for (int i = 1; i < argc; i++)
         printf(", %s", argv[i]);
     printf("\n");
